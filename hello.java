037094f97ec2ca2658b17d6732e06be8a80a5dad
ananimira <ananisamira05@gmail.com>
2023-03-31 14:28:13 +0200

    ecrire un code hello word en java

---
+++ hello.java
@@ -0,0 +1,16 @@
+// Définition d'une classe
+class HelloWorld 
+{ 
+    /* Votre programme commence par un appel à main(). 
+    Affiche "Hello, World" dans la fenêtre du terminal. */
+ 
+    public static void main(String args[]) 
+    { 
+        lolipop();
+        System.out.println("Super codeuse"); 
+    } 
+    
+    public int lolipop(){
+        System.out.println("Hello, World"); 
+    }
+}
